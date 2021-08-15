076bd78d01cfa09f0a5a696801932529fb7f38a3
Gaurav <51910127+gauravkr0071@users.noreply.github.com>
2021-08-15 21:51:04 +0530

    Create longest subarray with k sum.cpp

---
+++ ARRAY/longest subarray with k sum.cpp
@@ -0,0 +1,23 @@
+/*
+find the longest subbary with sum k,
+k can also be zero, return -1 if no array with sum k is present
+*/
+  unordered_map<int, int>u;
+  int len=INT_MIN;
+ for(int i=0;i<nums.size();i++)
+      {
+          temp+=nums[i];
+          
+          if(temp==k) len=i+1;
+          
+          if(u.find(temp-k)!=u.end())
+          len=max(len,i-u[temp-need]);
+          
+              if(u.find(temp)==u.end())
+                u[temp]=i;
+              
+          
+          
+      }
+   if(len==INT_MIN) return -1;
+     else return len;
