cc2f4d71cf0ad979ce76f6270aed87d7e5b9c7ca
Gaurav <51910127+gauravkr0071@users.noreply.github.com>
2021-08-15 22:25:13 +0530

    Update Merge Intervals.cpp

--- ARRAY/Merge Intervals.cpp
+++ ARRAY/Merge Intervals.cpp
@@ -2,6 +2,11 @@
 56. Merge Intervals
 Medium
 
+PROBLEM WITH SIMILIAR CONCEPTS:-
+252 Meeting Rooms
+253 Meeting Rooms II
+435 Non-overlapping Intervals 
+
 
 Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
